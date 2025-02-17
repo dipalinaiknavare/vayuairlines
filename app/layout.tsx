9c06e0f2079629e0c610ed0d0b6a5b977b08a691
Ranjeet Naiknavare <dipalimisal92@gmail.com>
2025-02-17 19:32:04 +0530

    Revert "Analytics"
    This reverts commit 969965657fd5d77d6ce0ad6213e86c72128382ce.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,22 +26,3 @@ export default function RootLayout({
 
 
 import './globals.css'
-import { Analytics } from '@vercel/analytics/next';
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
